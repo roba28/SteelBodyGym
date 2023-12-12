d76e9f9fa4ee8cc2232f598e2f16d313b352a3e3
RONNY AGUILAR <ronny199528@gmail.com>
2023-12-11 19:38:48 -0600

    Agregar archivos de proyecto.

---
+++ SteelBodyGym/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SteelBodyGym.Models;
+using System.Diagnostics;
+
+namespace SteelBodyGym.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult contacto()
+        {
+            return View();
+        }
+        public IActionResult Servicios()
+        {
+            return View();
+        }
+        public IActionResult AcercaDeNosotros()
+        {
+            return View();
+        }
+        public IActionResult Clases()
+        {
+            return View();
+        }
+        public IActionResult Ingresar()
+        {
+            return View();
+        }
+    }
+}
