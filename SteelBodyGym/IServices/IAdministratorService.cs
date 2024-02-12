ad1f8086e01af8623829b8d67497fca832db476f
Roda2406 <123038321+Roda2406@users.noreply.github.com>
2024-02-11 22:44:13 -0600

    Cambios en el apartado de Services y IServices agregando BodyMeasurementsUser hasta PaymentsPerUser

--- SteelBodyGym/IServices/IAdministratorService.cs
+++ SteelBodyGym/IServices/IAdministratorService.cs
@@ -5,5 +5,21 @@ namespace SteelBodyGym.IServices
     public interface IAdministratorService
     {
         List<Role> GetRoles();
+
+        List<BodyMeasurementsUser> GetBodyMeasurementsUsers();
+
+        List<City> GetCity();
+
+        List<County> GetCounties();
+
+        List<GymMachine> GetGymMachines();
+
+        List<GymRoutine> GetGymRoutine();
+
+        List<IdentificationType> GetIdentificationTypes();
+
+        List<MembershipRegistrationType> GetMembershipRegistrationTypes();
+
+        List<PaymentsPerUser> GetPaymentsPerUsers();
     }
 }
