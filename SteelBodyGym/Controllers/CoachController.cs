d363fec5b0b242a471609ec9dafd48d3ef45e298
Aaron <120622593+Aarongod1@users.noreply.github.com>
2024-02-27 19:19:17 -0600

    push aaron

---
+++ SteelBodyGym/Controllers/CoachController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SteelBodyGym.IServices;
+
+namespace SteelBodyGym.Controllers
+{
+    public class CoachController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        public IActionResult User()
+        {
+            return View();
+        }
+        public IActionResult GymMachines()
+        {
+            return View();
+        }
+        public IActionResult Routines()
+        {
+            return View();
+        }
+    }
+}
