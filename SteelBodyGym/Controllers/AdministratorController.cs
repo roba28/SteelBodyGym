255f0606a8a586e17f451bffc5244490bd483a6a
Aaron <120622593+Aarongod1@users.noreply.github.com>
2024-03-24 23:10:45 -0600

    cambios aaron

--- SteelBodyGym/Controllers/AdministratorController.cs
+++ SteelBodyGym/Controllers/AdministratorController.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SteelBodyGym.IServices;
 using SteelBodyGym.Model;
-using SteelBodyGym.Services;
+using System;
+using System.Linq;
 
 namespace SteelBodyGym.Controllers
 {
@@ -15,6 +16,7 @@ namespace SteelBodyGym.Controllers
             this._GlobalServices = GlobalServices;
             _AdministratorService = administratorService;
         }
+
         public IActionResult Users()
         {
             return View();
@@ -23,64 +25,50 @@ namespace SteelBodyGym.Controllers
         [HttpPost]
         public IActionResult GetProvinces()
         {
-
             var vResult = _GlobalServices.GetProvinces();
             return Ok(vResult);
-
         }
 
         [HttpPost]
         public IActionResult GetCounties()
         {
-
             var vResult = _GlobalServices.Getcounties();
             return Ok(vResult);
-
         }
 
         [HttpPost]
         public IActionResult GetCities()
         {
-
             var vResult = _GlobalServices.GetCities();
             return Ok(vResult);
-
         }
 
         [HttpPost]
         public IActionResult GetIdentificationType()
         {
-
             var vResult = _GlobalServices.GetIdentificationType();
             return Ok(vResult);
-
         }
 
         [HttpPost]
         public IActionResult GetUserSates()
         {
-
             var vResult = _GlobalServices.GetUserState();
             return Ok(vResult);
-
         }
 
         [HttpPost]
         public IActionResult GetRoles()
         {
-
             var vResult = _GlobalServices.GetRoles();
             return Ok(vResult);
-
         }
 
         [HttpPost]
-        public IActionResult UploadUser([FromBody] User user){
-
+        public IActionResult UploadUser([FromBody] User user)
+        {
             var vResult = _AdministratorService.UploadUser(user);
-            return Ok(new { data =vResult });
-
-
+            return Ok(new { data = vResult });
         }
 
         [HttpPost]
@@ -103,7 +91,5 @@ namespace SteelBodyGym.Controllers
 
             return Ok(result);
         }
-
-
     }
 }
